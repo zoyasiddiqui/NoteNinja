47a1df79a6bc9e408b088dcdedf17f6773e8e2e1
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-10-26 20:44:49 -0400

    Refactored Files into new sturcture

--- src/entity/CodeSnippet/CommonCodeSnippet.java
+++ src/entity/CodeSnippet/CommonCodeSnippet.java
@@ -2,4 +2,23 @@ package entity;
 
 class CommonCodeSnippet implements CodeSnippet {
 
+    @Override
+    public void setCode(String code) {
+
+    }
+
+    @Override
+    public void setOutput(String output) {
+
+    }
+
+    @Override
+    public String getCode() {
+        return null;
+    }
+
+    @Override
+    public String getOutput() {
+        return null;
+    }
 }
