3f943d0ca1594306de39b46e83bbb0f08986c8c5
Faisal <faisal.shaik@mail.utoronto.ca>
2023-10-26 21:43:21 -0400

    implemented LoginInputBoundary

---
+++ src/use_case/login/LoginDataAccessInterface.java
@@ -0,0 +1,4 @@
+package use_case.login;
+
+public class LoginDataAccessInterface {
+}
---
+++ src/use_case/login/LoginInputBoundary.java
@@ -0,0 +1,7 @@
+package use_case.login;
+
+import java.io.IOException;
+
+public interface LoginInputBoundary {
+    void execute() throws IOException;
+}
