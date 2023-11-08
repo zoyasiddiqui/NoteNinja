60a95863fbfefb6b467e961c32b61b4d1935fe01
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-11-08 13:43:55 -0500

    fixed implementation by replacing HTML.tag with tag

--- src/entity/Note/CommonNoteFactory.java
+++ src/entity/Note/CommonNoteFactory.java
@@ -1,12 +1,14 @@
 package entity.Note;
 
+import entity.Tag.Tag;
 import javax.swing.text.html.HTML;
 import java.util.List;
 
 class CommonNoteFactory implements NoteFactory{
 
     @Override
-    public void create(String name, List<HTML.Tag> tags, List<String> text) {
+    public void create(String name, List<Tag> tags, List<String> text) {
 
     }
+
 }
--- src/entity/Note/NoteFactory.java
+++ src/entity/Note/NoteFactory.java
@@ -1,10 +1,12 @@
 package entity.Note;
 
+import entity.Tag.Tag;
+
 import javax.swing.text.html.HTML;
 import java.util.List;
 
 public interface NoteFactory {
 
-    void create(String name, List<HTML.Tag> tags, List<String> text);
+    void create(String name, List<Tag> tags, List<String> text);
 
 }
