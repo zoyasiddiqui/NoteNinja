06845d8fac1a0093e8b6bd2f8055c5802c0120a8
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-11-12 17:02:35 -0500

    setup functions for data access objects

--- src/data_access/EditNoteDataAccessObject.java
+++ src/data_access/EditNoteDataAccessObject.java
@@ -3,7 +3,9 @@ package data_access;
 import entity.Note.Note;
 import use_case.create_note.CreateNoteDataAccessInterface;
 
-public class NoteDataAccessObject implements CreateNoteDataAccessInterface {
+public class EditNoteDataAccessObject implements CreateNoteDataAccessInterface {
+    public EditNoteDataAccessObject() {}
+
     @Override
     public void save(Note note) {
 
---
+++ src/data_access/ManageNotesDataAccessObject.java
@@ -0,0 +1,15 @@
+package data_access;
+
+import use_case.search_notes.SearchNotesAccessInterface;
+
+public class ManageNotesDataAccessObject implements SearchNotesAccessInterface {
+    @Override
+    public void findByName(String name) {
+
+    }
+
+    @Override
+    public void findByTag(String tagName) {
+
+    }
+}
