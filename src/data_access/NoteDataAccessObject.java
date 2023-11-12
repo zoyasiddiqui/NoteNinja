48a4146f089f22124fd63e5c5aea100c8d475718
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-11-12 13:51:05 -0500

    create class for note data access object and begin implementing necessary methods

---
+++ src/data_access/NoteDataAccessObject.java
@@ -0,0 +1,11 @@
+package data_access;
+
+import entity.Note.Note;
+import use_case.create_note.CreateNoteDataAccessInterface;
+
+public class NoteDataAccessObject implements CreateNoteDataAccessInterface {
+    @Override
+    public void save(Note note) {
+
+    }
+}
