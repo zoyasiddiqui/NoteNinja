6a5b8563de944ffac88eda6190348b9bb3218009
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-11-12 18:52:12 -0500

    setup output data for editing a note

--- src/use_case/edit_note/EditNoteOutputData.java
+++ src/use_case/edit_note/EditNoteOutputData.java
@@ -1,4 +1,31 @@
 package use_case.edit_note;
 
+import entity.Tag.Tag;
+
+import java.util.List;
+
 public class EditNoteOutputData {
+
+    private String title;
+    private List<Tag> tags;
+    private List<String> text;
+
+    public EditNoteOutputData(String title, List<Tag> tags, List<String> text) {
+        this.title = title;
+        this.tags = tags;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public List<Tag> getTags() {
+        return this.tags;
+    }
+
+    public List<String> getText() {
+        return this.text;
+    }
+
 }
