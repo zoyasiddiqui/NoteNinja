540023d6b737572e214ee82aadce12ab958dffac
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-11-12 19:24:57 -0500

    get rid of unecessary lines, causing problems

--- src/view/NoteEditorView.java
+++ src/view/NoteEditorView.java
@@ -14,32 +14,18 @@ import java.beans.PropertyChangeListener;
 public class NoteEditorView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "editing";
     private EditViewModel editViewModel;
-    // JLabel noteTitle;
-
-    JLabel noteTitle;
     final JButton saveNote;
 
     public NoteEditorView(EditViewModel editViewModel) {
         this.editViewModel = editViewModel;
         this.editViewModel.addPropertyChangeListener(this);
 
-        JLabel noteTitle = new JLabel();
-        noteTitle.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-        // not sure if we want this
-        // JLabel noteTitleInfo = new JLabel("Editing: ");
-        // noteTitle = new JLabel();
-
         JPanel buttons = new JPanel();
         saveNote = new JButton(editViewModel.LOGOUT_BUTTON_LABEL);
         buttons.add(saveNote);
 
         saveNote.addActionListener(this);
-
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-        this.add(noteTitle);
-        // this.add(noteTitleInfo);
         this.add(buttons);
     }
 
@@ -50,7 +36,5 @@ public class NoteEditorView extends JPanel implements ActionListener, PropertyCh
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        EditState state = (EditState) evt.getNewValue();
-        noteTitle.setText("Editing" + state.getNoteTitle());
     }
 }
