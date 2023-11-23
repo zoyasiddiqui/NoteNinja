d75d821322fe370470c6b45441f9b3269936ea73
Faisal <faisal.shaik@mail.utoronto.ca>
2023-11-23 17:11:18 -0500

    Changed note entity to have String type for text instead of List<String>. Appropriate adjustments made to implementing class CommonNote and EditNoteOutputData, and all respective factories.

--- src/entity/Note/Note.java
+++ src/entity/Note/Note.java
@@ -8,11 +8,11 @@ public interface Note {
 
     void setName(String title);
     void setTags(List<Tag> tags);
-    void setText(List<String> text);
+    void setText(String text);
     void save();
     void delete();
 
     String getName();
     List<Tag> getTags();
-    List<String> getText();
+    String getText();
 }
