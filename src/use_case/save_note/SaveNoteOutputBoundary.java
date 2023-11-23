a5246e21eb59d17c8bacec03752963785c48b7d6
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-11-23 17:21:21 -0500

    implemented necessary files for save note use case

--- src/use_case/save_note/SaveNoteOutputBoundary.java
+++ src/use_case/save_note/SaveNoteOutputBoundary.java
@@ -3,7 +3,7 @@ package use_case.save_note;
 import use_case.save_note.SaveNoteOutputData;
 
 public interface SaveNoteOutputBoundary {
-    void prepareSuccessView(SaveNoteOutputData usersDeleted);
+    void prepareSuccessView();
 
     void prepareFailView(String errormessage);
 }
