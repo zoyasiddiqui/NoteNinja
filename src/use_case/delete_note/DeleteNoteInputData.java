8bb0825999f585724e264902fbbddbf57c982abd
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-11-23 15:31:11 -0500

    implemented InputData and Interactor

---
+++ src/use_case/delete_note/DeleteNoteInputData.java
@@ -0,0 +1,4 @@
+package use_case.delete_note;
+
+public class DeleteNoteInputData {
+}
---
+++ src/use_case/delete_note/DeleteNoteInteractor.java
@@ -0,0 +1,24 @@
+package use_case.delete_note;
+
+import entity.Note.Note;
+import use_case.edit_note.EditNoteDataAccessInterface;
+import use_case.edit_note.EditNoteOutputBoundary;
+
+import java.io.IOException;
+
+public class DeleteNoteInteractor implements DeleteNoteInputBoundary {
+    private final EditNoteDataAccessInterface editNoteDataAccessInterface;
+    private final EditNoteOutputBoundary editNotePresenter;
+
+    public DeleteNoteInteractor(EditNoteDataAccessInterface editNoteDataAccessInterface,
+                                EditNoteOutputBoundary editNotePresenter) {
+        this.editNoteDataAccessInterface = editNoteDataAccessInterface;
+        this.editNotePresenter = editNotePresenter;
+    }
+
+
+    @Override
+    public void execute() throws IOException {
+
+    }
+}
