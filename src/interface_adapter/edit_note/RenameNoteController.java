5061b364412a57c0f95d5153367aec0c2a6e403b
Faisal <faisal.shaik@mail.utoronto.ca>
2023-11-23 05:50:15 -0500

    Implemented RenameNoteController and RenameNoteInputData

--- src/interface_adapter/edit_note/RenameNoteController.java
+++ src/interface_adapter/edit_note/RenameNoteController.java
@@ -1,4 +1,16 @@
 package interface_adapter.edit_note;
 
+import use_case.rename_note.RenameNoteInputBoundary;
+import use_case.rename_note.RenameNoteInputData;
+
 public class RenameNoteController {
+    final RenameNoteInputBoundary renameInteractor;
+
+    public RenameNoteController (RenameNoteInputBoundary renameInteractor) {
+        this.renameInteractor = renameInteractor;
+    }
+
+    public void execute(String noteTitle) {
+        RenameNoteInputData renameNoteInputData = new RenameNoteInputData(noteTitle);
+    }
 }
