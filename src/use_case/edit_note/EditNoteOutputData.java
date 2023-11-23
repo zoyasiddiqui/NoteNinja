1c66642e144ca7b67b1fc0655c9ac07260db055d
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-11-23 15:45:16 -0500

    added note as an attribute / part of state, to keep track of what note we're editing

--- src/use_case/edit_note/EditNoteOutputData.java
+++ src/use_case/edit_note/EditNoteOutputData.java
@@ -1,16 +1,19 @@
 package use_case.edit_note;
 
+import entity.Note.Note;
 import entity.Tag.Tag;
 
 import java.util.List;
 
 public class EditNoteOutputData {
 
+    private Note note;
     private String title;
     private List<Tag> tags;
     private List<String> text;
 
-    public EditNoteOutputData(String title, List<Tag> tags, List<String> text) {
+    public EditNoteOutputData(Note note, String title, List<Tag> tags, List<String> text) {
+        this.note = note;
         this.title = title;
         this.tags = tags;
         this.text = text;
