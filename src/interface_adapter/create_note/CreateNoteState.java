1d56828fe86fe9ec7e7a7029a9f42b1665fcafbf
Tanish Roy <tanishroy1963@gmail.com>
2023-11-23 18:40:35 -0500

    Removed tag and code_snippet usecase instances from interface_adapter.create_note, CreateNoteState, CreateNoteController

--- src/interface_adapter/create_note/CreateNoteState.java
+++ src/interface_adapter/create_note/CreateNoteState.java
@@ -1,18 +1,14 @@
 package interface_adapter.create_note;
 
-import entity.Tag.Tag;
-
 import java.util.List;
 
 public class CreateNoteState {
     private String title;
-    private List<Tag> tags;
     private List<String> text;
 
     public CreateNoteState() {}
     public CreateNoteState(CreateNoteState copy) {
         this.title = copy.title;
-        this.tags = copy.tags;
         this.text = copy.text;
     }
 
@@ -22,14 +18,6 @@ public class CreateNoteState {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public List<Tag> getTags() {
-        return this.tags;
-    }
-
-    public void setTags(List<Tag> tags) {
-        this.tags = tags;
     }
 
     public List<String> getText() {
