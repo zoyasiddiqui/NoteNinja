e5085dae404365dcbbe1d1b7d3ae3f40388f7a11
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-11-23 02:23:24 -0500

    Implemented DeleteNoteState and DeleteNoteViewModel

--- src/interface_adapter/delete_note/DeleteNoteViewModel.java
+++ src/interface_adapter/delete_note/DeleteNoteViewModel.java
@@ -3,21 +3,35 @@ package interface_adapter.delete_note;
 import interface_adapter.ViewModel;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class DeleteNoteViewModel extends ViewModel {
+    public final String TITLE_LABEL = "Deleting View";
 
-    //TODO: modify constructor as needed
-    public DeleteNoteViewModel(String viewName) {
-        super(viewName);
+    private DeleteNoteViewModel state = new DeleteNoteViewModel();
+
+    public static final String DELETE_BUTTON_LABEL = "Delete";
+    private String noteTitle;
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public DeleteNoteViewModel() {
+        super("delete view");
     }
 
-    @Override
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
     public void firePropertyChanged() {
-
+        support.firePropertyChange("state", null, this.state);
     }
 
-    @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
-
+        support.addPropertyChangeListener(listener);
     }
 }
