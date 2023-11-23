8b0dea5a209ec12fbaaf0e047c44e3d8c9c6c21f
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-11-23 14:58:11 -0500

    implemented dao using spreadsheet api

--- src/data_access/EditNoteDataAccessObject.java
+++ src/data_access/EditNoteDataAccessObject.java
@@ -1,64 +1,40 @@
 package data_access;
 
 import entity.Note.Note;
-import entity.Note.NoteFactory;
-import entity.Tag.CommonTag;
-import entity.Tag.Tag;
 import use_case.create_note.CreateNoteDataAccessInterface;
+import use_case.delete_note.DeleteNoteDataAccessInterface;
 import use_case.edit_note.EditNoteDataAccessInterface;
 import use_case.search_notes.SearchNotesAccessInterface;
 
-import java.io.*;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class EditNoteDataAccessObject implements CreateNoteDataAccessInterface, SearchNotesAccessInterface, EditNoteDataAccessInterface {
-    private final File noteFile;
-    private String noteName;
-    private Map<String, Tag> tags = new HashMap<>();
-    private Map<String, Note> notes = new HashMap<>();
-    private final NoteFactory noteFactory;
-    public EditNoteDataAccessObject(String path, NoteFactory noteFactory, String noteName) {
-        this.noteFactory = noteFactory;
-        this.noteName = noteName;
+public class EditNoteDataAccessObject implements CreateNoteDataAccessInterface, EditNoteDataAccessInterface, DeleteNoteDataAccessInterface, SearchNotesAccessInterface {
 
-        //create note object to put all text in
-        this.noteFile = new File(path);
+    String url = "https://api.apispreadsheets.com/data/9pYIi0HNV5wCWDYX/";
 
-        //create initial tag
-        Tag nameTag = new CommonTag(this.noteName);
-        this.tags.put(this.noteName, nameTag);
-
-        ArrayList<Tag> tagList = new ArrayList<Tag>();
-        tagList.add(nameTag);
-        ArrayList<String> text = new ArrayList<String>();
-        this.notes.put(this.noteName, this.noteFactory.create(this.noteName, tagList, text));
-    }
+    public EditNoteDataAccessObject() {}
 
     @Override
     public void save(Note note) throws IOException {
-        //note that we update all three attributes of our note object
-
-        BufferedWriter writer = new BufferedWriter(new FileWriter(this.noteFile));
-
-        //update our name
-        this.noteName = note.getName();
-        writer.write(this.noteName);
-        writer.newLine();
-
-        //update our list of tags
-        for (Tag tag : note.getTags()) {
-            this.tags.put(tag.getAttribute(), tag);
-        }
-        //update our text
-        for (String line : note.getText()) {
-            writer.write(line);
-            writer.newLine();
-        }
-
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-type", "application/json");
+        String query = "select * from 9pYIi0HNV5wCWDYX where Note Name='Note Content'";
+        String body = "{\"data\": {\"Name\": \"\", \"ID\":\"\", \"Content\":\"New note content\"} \"query\":"+query+"}";
+        connection.setDoOutput(true);
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        writer.write(body);
+        writer.flush();
         writer.close();
+    }
+
+    @Override
+    public void delete(Note note) throws IOException {
+
     }
 
     @Override
