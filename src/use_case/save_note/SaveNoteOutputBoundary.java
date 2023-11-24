cb3bb9cfb0161e8d46e66282646642f76817656f
Faisal <faisal.shaik@mail.utoronto.ca>
2023-11-23 19:58:53 -0500

    Got rid of SaveNotePresenter as we do not need any presenter for saving notes, we will not make any view switches but just execute the save use_case and stay on EditNoteView

--- src/use_case/save_note/SaveNoteOutputBoundary.java
+++
@@ -1,9 +0,0 @@
-package use_case.save_note;
-
-import use_case.save_note.SaveNoteOutputData;
-
-public interface SaveNoteOutputBoundary {
-    void prepareSuccessView();
-
-    void prepareFailView(String errormessage);
-}
