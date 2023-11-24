8efc2f06f021a622722ecbb4f1174e65cb22818e
Faisal <faisal.shaik@mail.utoronto.ca>
2023-11-24 03:57:40 -0500

    Instead of passing a string as an argument for createNoteInteractor.execute(), we now create a createNoteInputData.

--- src/interface_adapter/create_note/CreateNoteController.java
+++ src/interface_adapter/create_note/CreateNoteController.java
@@ -1,6 +1,7 @@
 package interface_adapter.create_note;
 import entity.Note.Note;
 import use_case.create_note.CreateNoteInputBoundary;
+import use_case.create_note.CreateNoteInputData;
 import use_case.create_note.CreateNoteInteractor;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class CreateNoteController{
     }
 
     public void execute(String name) throws IOException {
-        this.createNoteInteractor.execute(name);
+        CreateNoteInputData createNoteInputData = new CreateNoteInputData(name);
+        this.createNoteInteractor.execute(createNoteInputData);
     }
 }
