ae7f6dbe90a650fdfbf4280ca37ab84156388894
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-11-29 09:25:18 -0500

    Created and implemented DAo for CodeSnippet

---
+++ src/data_access/CreateCodeSnippetDataAccessObject.java
@@ -0,0 +1,62 @@
+package data_access;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Objects;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class CreateCodeSnippetDataAccessObject {
+
+    public StringBuilder runCode(String code) {
+        String url = "https://glot.io/api/run/python/latest";
+        String myKey = readApiKey();
+
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Token " + myKey);
+            connection.setRequestProperty("Content-type", "application/json");
+
+            // Build the request body
+            String body = "{\"files\": [{\"name\": \"main.py\", \"content\": \"" + code + "\"}]}";
+            connection.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(body);
+            writer.flush();
+            writer.close();
+
+            // Get the response
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Extract the output from the response
+            String output = response.toString().split(",")[0].split(":")[1];
+            return new StringBuilder(output);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String readApiKey() {
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("C:\\path\\to\\your\\config.properties")) {
+            properties.load(input);
+            return properties.getProperty("api.key");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
