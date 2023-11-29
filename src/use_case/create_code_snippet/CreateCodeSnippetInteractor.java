e0467701d25e78c0672ab8eefb3f94610673910a
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-11-28 21:58:12 -0500

    Implemented CodeSnippetInteractor and OutputBoundary

--- src/use_case/create_code_snippet/CreateCodeSnippetInteractor.java
+++ src/use_case/create_code_snippet/CreateCodeSnippetInteractor.java
@@ -1,2 +1,32 @@
-package use_case.create_code_snippet;public class CreateCodeSnippetInteractor {
+package use_case.create_code_snippet;
+
+import use_case.edit_note.EditNoteDataAccessInterface;
+import use_case.edit_note.EditNoteOutputBoundary;
+
+import java.io.IOException;
+
+public class CreateCodeSnippetInteractor implements CreateCodeSnippetInputBoundary {
+    private final CreateCodeSnippetDataAccessInterface createCodeSnippetDataAccessObject;
+    private final EditNoteDataAccessInterface editNoteDAO;
+    private final EditNoteOutputBoundary editNotePresenter;
+
+    public CreateCodeSnippetInteractor(CreateCodeSnippetDataAccessInterface createCodeSnippetDataAccessObject, EditNoteDataAccessInterface editNoteDAO, EditNoteOutputBoundary editNotePresenter) {
+        this.createCodeSnippetDataAccessObject = createCodeSnippetDataAccessObject;
+        this.editNoteDAO = editNoteDAO;
+        this.editNotePresenter = editNotePresenter;
+    }
+
+    @Override
+    public void execute(String code) throws IOException {
+        // Use the DAO to execute the code and get the response
+        StringBuilder output = createCodeSnippetDataAccessObject.executeCode(code);
+
+        // Update the text with the code snippet output
+        String text = editNoteDAO.getCurrentText() + new String(output);
+        editNoteDAO.setCurrentText(text);
+
+        // Prepare the output data and notify the presenter
+        CreateCodeSnippetOutputData createCodeSnippetOutputData = new CreateCodeSnippetOutputData(text);
+        editNotePresenter.prepareSuccessView(createCodeSnippetOutputData);
+    }
 }
--- src/use_case/create_code_snippet/CreateCodeSnippetOutputBoundary.java
+++ src/use_case/create_code_snippet/CreateCodeSnippetOutputBoundary.java
@@ -1,2 +1,9 @@
-package use_case.create_code_snippet;public interface CreateCodeSnippetOutputBoundary {
+package use_case.create_code_snippet;
+
+public interface CreateCodeSnippetOutputBoundary {
+
+    void prepareSuccessView(CreateCodeSnippetOutputData response);
+
+    void prepareFailView(String error);
+
 }
