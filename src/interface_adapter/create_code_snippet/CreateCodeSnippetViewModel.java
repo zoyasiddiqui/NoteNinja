dd33507de4a450803bd5b5573f13e0dcf1ba5b38
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-11-28 21:54:39 -0500

    Implemented CodeSnippet Controller, Presenter and ViewModel

--- src/interface_adapter/create_code_snippet/CreateCodeSnippetViewModel.java
+++ src/interface_adapter/create_code_snippet/CreateCodeSnippetViewModel.java
@@ -1,2 +1,25 @@
-package interface_adapter.create_code_snippet;public class CreateCodeSnippetViewModel {
+package interface_adapter.create_code_snippet;
+
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class CreateCodeSnippetViewModel extends ViewModel {
+
+    // TODO: Modify constructor as needed
+    public CreateCodeSnippetViewModel(String viewName) {
+        super(viewName);
+    }
+
+    @Override
+    public void firePropertyChanged() {
+        // Implement the logic to notify property change
+        // You may need to specify which property has changed and notify listeners accordingly
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        // Implement the logic to add property change listeners
+    }
 }
