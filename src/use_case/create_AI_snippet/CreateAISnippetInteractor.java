aa9411d25f31f36156a2b4e0b3772f62aaab026d
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-11-30 20:08:01 -0500

    Fixed bugs in Aisnippet InputBoundary and Interactor

--- src/use_case/create_AI_snippet/CreateAISnippetInteractor.java
+++ src/use_case/create_AI_snippet/CreateAISnippetInteractor.java
@@ -1,27 +1,31 @@
 package use_case.create_AI_snippet;
 
+import interface_adapter.edit_note.EditNoteState;
 import use_case.edit_note.EditNoteDataAccessInterface;
 import use_case.edit_note.EditNoteOutputBoundary;
+
+import java.io.IOException;
 
 public class CreateAISnippetInteractor implements CreateAISnippetInputBoundary{
     private final CreateAISnippetDataAccessInterface createAISnippetDataAccessObject;
     private final EditNoteDataAccessInterface editNoteDAO;
-    private final EditNoteOutputBoundary editNotePresenter;
+    private final CreateAISnippetOutputBoundary createAISnippetPresenter;
 
-    public CreateAISnippetInteractor(CreateAISnippetDataAccessInterface createAISnippetDataAccessObject, EditNoteDataAccessInterface editNoteDAO, EditNoteOutputBoundary editNotePresenter) {
+    public CreateAISnippetInteractor(CreateAISnippetDataAccessInterface createAISnippetDataAccessObject, EditNoteDataAccessInterface editNoteDAO, CreateAISnippetOutputBoundary createAISnippetPresenter) {
         this.createAISnippetDataAccessObject = createAISnippetDataAccessObject;
         this.editNoteDAO = editNoteDAO;
-        this.editNotePresenter = editNotePresenter;
+        this.createAISnippetPresenter = createAISnippetPresenter;
     }
 
     @Override
-    public void execute(String prompt, String noteText) {
+    public void execute(String prompt, String noteText, EditNoteState editNoteState) throws IOException {
         //use the DAO to use the API to give us back the AI response
         StringBuilder response = createAISnippetDataAccessObject.getResponse(prompt);
         String text = noteText + new String(response);
+        editNoteDAO.updateNote(editNoteState.getNoteID(), noteText, editNoteState.getNoteTitle());
         // create a note entity using the InputData
 
         CreateAISnippetOutputData createAISnippetOutputData = new CreateAISnippetOutputData(text);
-        editNotePresenter.prepareSnippetAdded(createAISnippetOutputData);
+        createAISnippetPresenter.prepareSuccessView(createAISnippetOutputData);
     }
 }
