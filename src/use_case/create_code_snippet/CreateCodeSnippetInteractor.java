7886a3268761bff989aea5735203bf0646308a74
tanishboiiii <tanishroy1963@gmail.com>
2023-12-01 05:38:58 -0500

    CreateCodeSnippetInteractor now creates an editNoteOutputData object and calls editPresenter.prepareNote().

--- src/use_case/create_code_snippet/CreateCodeSnippetInteractor.java
+++ src/use_case/create_code_snippet/CreateCodeSnippetInteractor.java
@@ -1,7 +1,9 @@
 package use_case.create_code_snippet;
 
+import interface_adapter.edit_note.EditNoteState;
 import use_case.edit_note.EditNoteDataAccessInterface;
 import use_case.edit_note.EditNoteOutputBoundary;
+import use_case.edit_note.EditNoteOutputData;
 
 import java.io.IOException;
 
@@ -20,16 +22,17 @@ public class CreateCodeSnippetInteractor implements CreateCodeSnippetInputBounda
     @Override
     public void execute(CreateCodeSnippetInputData createCodeSnippetInputData) throws IOException {
 
+        EditNoteState editNoteState = createCodeSnippetInputData.getNoteState();
         String code = createCodeSnippetInputData.getCode();
         String text = createCodeSnippetInputData.getText();
         // use the DAO to execute the code and get the response
         StringBuilder output = createCodeSnippetDataAccessObject.executeCode(code);
 
         // update current text with new text. note we do NOT retrieve the current text from our editNoteDAO, we get it from note state
-
         String newText = text + new String(output);
 
         // prepare the output data and notify the presenter
-        editNotePresenter.prepareTextChange(newText);
+        EditNoteOutputData editNoteOutputData = new EditNoteOutputData(editNoteState.getNoteID(), editNoteState.getNoteTitle(), newText);
+        editNotePresenter.prepareNote(editNoteOutputData);
     }
 }
