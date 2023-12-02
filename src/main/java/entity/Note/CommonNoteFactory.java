2e46c06724eb43ca0023c14db6f870b47c69b08f
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-02 06:52:36 -0500

    removed note counting and ID giving system from CommonNoteFactory. added noteID argument in the creation of notes now. the IDs are being assigned through the interactors as they should be, and the notecount value is tracked in DAO

--- src/main/java/entity/Note/CommonNoteFactory.java
+++ src/main/java/entity/Note/CommonNoteFactory.java
@@ -5,11 +5,8 @@ import java.util.List;
 
 public class CommonNoteFactory implements NoteFactory{
 
-    private static int noteID = -1;
-
     @Override
-    public Note create(String name, String text) {
-        noteID += 1;
+    public Note create(String name, String text, int noteID) {
         System.out.println(noteID);
         return new CommonNote(name, text, noteID);
     }
