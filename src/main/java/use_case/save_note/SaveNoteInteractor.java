6e4f9b93878e7b4c0e059db1a8eaadde343e376f
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-02 07:14:49 -0500

    significant additions to savenote usecase. we now call noteFactory.create() in SaveNoteInteractor.execute() insteead of createNoteInteractor.execute(). this is because the job of createNoteInteractor isnt to write any new DAO files but to simply create a blank noteState and assign it an ID then bring it to view. the actual persistence should be handlede by SaveNoteInteractor. We now have a case for if the note does not already exist; we create it by making a note entity and sending it to the DAO.

--- src/main/java/use_case/save_note/SaveNoteInteractor.java
+++ src/main/java/use_case/save_note/SaveNoteInteractor.java
@@ -2,8 +2,6 @@ package use_case.save_note;
 
 import entity.Note.Note;
 import entity.Note.NoteFactory;
-import interface_adapter.edit_note.EditNotePresenter;
-import interface_adapter.edit_note.EditNoteState;
 import use_case.create_note.CreateNoteDataAccessInterface;
 import use_case.edit_note.EditNoteDataAccessInterface;
 import use_case.edit_note.EditNoteOutputBoundary;
@@ -13,37 +11,37 @@ import java.io.IOException;
 
 public class SaveNoteInteractor implements SaveNoteInputBoundary{
 
-    private final EditNoteDataAccessInterface editNoteDataAccessInterface;
-    private final CreateNoteDataAccessInterface createNoteDataAccessInterface;
+    private final EditNoteDataAccessInterface noteDataAccessObject;
     private final NoteFactory noteFactory;
     private final EditNoteOutputBoundary editNotePresenter;
 
-    public SaveNoteInteractor(EditNoteDataAccessInterface editNoteDataAccessInterface,
-                              CreateNoteDataAccessInterface createNoteDataAccessInterface,
+    public SaveNoteInteractor(EditNoteDataAccessInterface noteDataAccessObject,
                               NoteFactory noteFactory,
                               EditNoteOutputBoundary editNotePresenter) {
-        this.editNoteDataAccessInterface = editNoteDataAccessInterface;
-        this.createNoteDataAccessInterface = createNoteDataAccessInterface;
+        this.noteDataAccessObject = noteDataAccessObject;
         this.noteFactory = noteFactory;
         this.editNotePresenter = editNotePresenter;
     }
 
     @Override
     public void execute(SaveNoteInputData saveNoteInputData) throws IOException {
-        // create a note entity using the InputData
         int noteID = saveNoteInputData.getNoteID();
+        // create a note entity using the InputData
         String noteText = saveNoteInputData.getNoteText();
         String noteTitle = saveNoteInputData.getNoteTitle();
 
-        if (editNoteDataAccessInterface.existsByID(noteID)) { // note already exists
+        if (noteDataAccessObject.existsByID(noteID)) { // note already exists
             System.out.println("Note exists.");
 
-            editNoteDataAccessInterface.updateNote(noteID, noteText, noteTitle);
-            System.out.println(editNoteDataAccessInterface.getCurrentText());
+            noteDataAccessObject.updateNote(noteID, noteText, noteTitle);
+            System.out.println(noteDataAccessObject.getCurrentText());
 
             EditNoteOutputData editNoteOutputData = new EditNoteOutputData(noteID, noteTitle, noteText);
             editNotePresenter.prepareNote(editNoteOutputData);
 
+        } else {
+            Note note = noteFactory.create(noteTitle, noteText, noteID);
+            ((CreateNoteDataAccessInterface) noteDataAccessObject).create(note);
         }
     }
 }
