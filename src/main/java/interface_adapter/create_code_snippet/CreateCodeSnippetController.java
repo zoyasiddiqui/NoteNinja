eb8bd1453119a1df500697b6c9f28f352c99c114
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-02 15:32:40 -0500

    Added comments for AI interface adapter and Code

--- src/main/java/interface_adapter/create_code_snippet/CreateCodeSnippetController.java
+++ src/main/java/interface_adapter/create_code_snippet/CreateCodeSnippetController.java
@@ -1,21 +1,30 @@
+// Package declaration
 package interface_adapter.create_code_snippet;
 
+// Import statements for various classes and interfaces
 import interface_adapter.edit_note.EditNoteState;
 import use_case.create_code_snippet.CreateCodeSnippetInputBoundary;
 import use_case.create_code_snippet.CreateCodeSnippetInputData;
 
 import java.io.IOException;
 
+// Class declaration for CreateCodeSnippetController
 public class CreateCodeSnippetController {
 
+    // Private member variable to hold CreateCodeSnippetInputBoundary instance
     private final CreateCodeSnippetInputBoundary createCodeSnippetInteractor;
 
+    // Constructor to initialize CreateCodeSnippetController with CreateCodeSnippetInputBoundary
     public CreateCodeSnippetController(CreateCodeSnippetInputBoundary createCodeSnippetInteractor) {
         this.createCodeSnippetInteractor = createCodeSnippetInteractor;
     }
 
+    // Method to execute the creation of a code snippet
     public void execute(String code, String noteText, String noteTitle, int noteID) throws IOException {
+        // Create CreateCodeSnippetInputData instance with provided parameters
         CreateCodeSnippetInputData createCodeSnippetInputData = new CreateCodeSnippetInputData(code, noteText, noteTitle, noteID);
+
+        // Execute the create code snippet use case
         this.createCodeSnippetInteractor.execute(createCodeSnippetInputData);
     }
 }
