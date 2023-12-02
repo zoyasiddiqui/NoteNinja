b3e9d0228de694d373169965dbadb9ba4f45dddb
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-02 15:36:36 -0500

    Added comments in create_note and delete_note Interaface Adapters

--- src/main/java/interface_adapter/create_note/CreateNoteController.java
+++ src/main/java/interface_adapter/create_note/CreateNoteController.java
@@ -1,18 +1,27 @@
+// Package declaration
 package interface_adapter.create_note;
+
+// Import statements for various classes and interfaces
 import use_case.create_note.CreateNoteInputBoundary;
 import use_case.create_note.CreateNoteInputData;
 
 import java.io.IOException;
 
-public class CreateNoteController{
+// Class declaration for CreateNoteController
+public class CreateNoteController {
+    // Private member variable to hold CreateNoteInputBoundary instance
     final CreateNoteInputBoundary createNoteInteractor;
 
+    // Constructor to initialize CreateNoteController with CreateNoteInputBoundary
     public CreateNoteController(CreateNoteInputBoundary createNoteInteractor) {
         this.createNoteInteractor = createNoteInteractor;
     }
 
+    // Method to execute the creation of a new note
     public void execute(String noteTitle, String noteText) throws IOException {
+        // Create CreateNoteInputData instance with provided noteTitle and noteText
         CreateNoteInputData createNoteInputData = new CreateNoteInputData(noteTitle, noteText);
+        // Execute the creation of the note using CreateNoteInteractor
         this.createNoteInteractor.execute(createNoteInputData);
     }
 }
