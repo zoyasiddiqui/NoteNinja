574161bbf00bbfde5d9fa420655dc65642cca59a
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-02 04:25:22 -0500

    fixed createcodesnippetinputdata. before we were passing in the whole note state directly, which was fine but inconsistent with the rest of our design

--- src/main/java/use_case/create_code_snippet/CreateCodeSnippetInteractor.java
+++ src/main/java/use_case/create_code_snippet/CreateCodeSnippetInteractor.java
@@ -21,34 +21,36 @@ public class CreateCodeSnippetInteractor implements CreateCodeSnippetInputBounda
     @Override
     public void execute(CreateCodeSnippetInputData createCodeSnippetInputData) throws IOException {
 
-        EditNoteState editNoteState = createCodeSnippetInputData.getNoteState();
         String code = createCodeSnippetInputData.getCode();
-        String text = createCodeSnippetInputData.getText();
+        String currentText = createCodeSnippetInputData.getNoteText();
         // use the DAO to execute the code and get the response
         StringBuilder output = createCodeSnippetDataAccessObject.executeCode(code);
 
-        // update current text with new text. note we do NOT retrieve the current text from our editNoteDAO, we get it from note state
+        // update current currentText with new currentText. note we do NOT retrieve the current currentText from our editNoteDAO, we get it from note state
         String codeReturn = new String(output);
         System.out.println(codeReturn);
 
         // set appropriate amount of newline characters
         String newText = "\n\n";
-        int l = text.length();
-        if (text.isEmpty()) {
+        int l = currentText.length();
+        if (currentText.isEmpty()) {
             newText = "";
-        } else if (text.charAt(l - 1) == '\n') {
+        } else if (currentText.charAt(l - 1) == '\n') {
             System.out.println("bababooey");
             newText = "\n";
         } if (l > 2) {
-            if (text.charAt(l - 1) == '\n' && text.charAt(l - 2) == '\n') {
+            if (currentText.charAt(l - 1) == '\n' && currentText.charAt(l - 2) == '\n') {
                 newText = "";
             }
         }
 
-        newText = text + newText + "Ran code snippet:" + dashes + code + dashes + codeReturn;
+        newText = currentText + newText + "Ran code snippet:" + dashes + code + dashes + codeReturn;
 
         // prepare the output data and notify the presenter
-        EditNoteOutputData editNoteOutputData = new EditNoteOutputData(editNoteState.getNoteID(), editNoteState.getNoteTitle(), newText);
+        EditNoteOutputData editNoteOutputData = new EditNoteOutputData(
+                createCodeSnippetInputData.getNoteID(),
+                createCodeSnippetInputData.getNoteTitle(),
+                newText);
         editNotePresenter.prepareNote(editNoteOutputData);
     }
 }
