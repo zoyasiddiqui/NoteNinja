abf4f18f5a3b3d105aa1602040749ac14510a41d
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-03 05:13:07 -0500

    implemented create_code_snippet tests for use_case

--- src/test/java/use_case/create_code_snippet/CreateCodeSnippetInteractorTest.java
+++ src/test/java/use_case/create_code_snippet/CreateCodeSnippetInteractorTest.java
@@ -1,7 +1,58 @@
 package use_case.create_code_snippet;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-class CreateCodeSnippetInteractorTest {
+import org.junit.jupiter.api.Test;
+import use_case.edit_note.EditNoteOutputBoundary;
 
+import java.io.IOException;
+
+public class CreateCodeSnippetInteractorTest {
+
+    @Test
+    void testExecute() throws IOException {
+        // Mock dependencies
+        CreateCodeSnippetDataAccessInterface dataAccessObject = mock(CreateCodeSnippetDataAccessInterface.class);
+        EditNoteOutputBoundary editNotePresenter = mock(EditNoteOutputBoundary.class);
+
+        // Create an instance of CreateCodeSnippetInputData
+        CreateCodeSnippetInputData inputData = new CreateCodeSnippetInputData("Sample Code", "Existing Text",
+                "Note Title", 123);
+
+        // Create an instance of CreateCodeSnippetInteractor using the mock dependencies
+        CreateCodeSnippetInteractor interactor = new CreateCodeSnippetInteractor(dataAccessObject, editNotePresenter);
+
+        // Define the behavior of the mock data access object
+        when(dataAccessObject.executeCode("Sample Code")).thenReturn(new StringBuilder("Code Execution Result"));
+
+        // Call the execute method of the interactor
+        interactor.execute(inputData);
+
+        // Verify that the editNotePresenter's prepareNote method was called with the expected EditNoteOutputData
+        verify(editNotePresenter).prepareNote(argThat(outputData ->
+                outputData.getNoteID() == 123 &&
+                        outputData.getNoteTitle().equals("Note Title") &&
+                        outputData.getNoteText().contains("Ran code snippet:\n---\nSample Code\n---\nCode Execution Result")));
+    }
+
+    @Test
+    void testCalculateNewText() {
+        // Create an instance of CreateCodeSnippetInteractor
+        CreateCodeSnippetInteractor interactor = new CreateCodeSnippetInteractor(null, null);
+
+        // Test when existing text is empty
+        assertEquals("\n\n", interactor.calculateNewText(""));
+
+        // Test when existing text ends with a single newline character
+        assertEquals("\n", interactor.calculateNewText("Existing Text\n"));
+
+        // Test when existing text ends with two newline characters
+        assertEquals("", interactor.calculateNewText("Existing Text\n\n"));
+
+        // Test when existing text is not empty and doesn't end with newline characters
+        assertEquals("\n\n", interactor.calculateNewText("Existing Text"));
+    }
+
+    // Additional test cases for specific scenarios can be added as needed
 }
