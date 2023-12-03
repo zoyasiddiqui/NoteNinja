6ddeea57c9d20acedd6cf4f3d067ab3d851adfbc
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-03 17:33:02 -0500

    Updated tests for RenameController, Backmenuinteractor and RenameNoteinteractor

--- src/test/java/use_case/back_menu/BackMenuInteractorTest.java
+++ src/test/java/use_case/back_menu/BackMenuInteractorTest.java
@@ -1,37 +1,37 @@
 package use_case.back_menu;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import use_case.edit_note.EditNoteOutputBoundary;
+
+import static org.mockito.Mockito.verify;
 
 public class BackMenuInteractorTest {
 
     @Test
     void testExecute() {
-        // Mock the EditNoteOutputBoundary presenter
-        EditNoteOutputBoundary editNotePresenter = Mockito.mock(EditNoteOutputBoundary.class);
+        // Mock the BackMenuOutputBoundary presenter
+        BackMenuOutputBoundary backMenuPresenter = Mockito.mock(BackMenuOutputBoundary.class);
 
         // Create an instance of BackMenuInteractor with the mocked presenter
-        BackMenuInteractor backMenuInteractor = new BackMenuInteractor(editNotePresenter);
+        BackMenuInteractor backMenuInteractor = new BackMenuInteractor(backMenuPresenter);
 
         // Call the execute method
         backMenuInteractor.execute();
 
         // Verify that the prepareBackMenu method on the presenter is called
-        Mockito.verify(editNotePresenter, Mockito.times(1)).prepareBackMenu();
+        verify(backMenuPresenter, Mockito.times(1)).prepareBackMenu();
     }
 
     @Test
     void testConstructor() {
-        // Mock the EditNoteOutputBoundary presenter
-        EditNoteOutputBoundary editNotePresenter = Mockito.mock(EditNoteOutputBoundary.class);
+        // Mock the BackMenuOutputBoundary presenter
+        BackMenuOutputBoundary backMenuPresenter = Mockito.mock(BackMenuOutputBoundary.class);
 
         // Create an instance of BackMenuInteractor with the mocked presenter
-        BackMenuInteractor backMenuInteractor = new BackMenuInteractor(editNotePresenter);
+        BackMenuInteractor backMenuInteractor = new BackMenuInteractor(backMenuPresenter);
 
-        // Assert that the editNotePresenter instance variable is set
-        assertNotNull(backMenuInteractor.editNotePresenter);
-        assertEquals(editNotePresenter, backMenuInteractor.editNotePresenter);
+        // Assert that the backMenuPresenter instance variable is set
+        assert backMenuInteractor.backMenuPresenter != null;
+        assert backMenuInteractor.backMenuPresenter.equals(backMenuPresenter);
     }
 }
