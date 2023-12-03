1e233c8445f8abb0ca4d85d01847117edb9145fa
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-03 08:29:48 -0500

    implemented RetrieveInputBoundary

---
+++ src/main/java/use_case/retrieve_usecase/RetrieveInputBoundary.java
@@ -0,0 +1,8 @@
+package use_case.retrieve_usecase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface RetrieveInputBoundary {
+    ArrayList execute() throws IOException;
+}
