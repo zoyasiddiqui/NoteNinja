0167d2c209860d58e81209f8ca91961d356ae7ff
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-02 22:06:21 -0500

    Removed unneeded files and finished commenting last use case

--- src/main/java/use_case/search_notes/SearchNotesAccessInterface.java
+++ src/main/java/use_case/search_notes/SearchNotesAccessInterface.java
@@ -1,11 +1,13 @@
+// Package declaration indicating the location of the interface within the project structure
 package use_case.search_notes;
 
+// Import statement for the Note entity
 import entity.Note.Note;
 
-import java.util.List;
-
+// Definition of the SearchNotesAccessInterface interface
 public interface SearchNotesAccessInterface {
 
+    // Method signature for finding a Note by its title
     Note findByTitle(String noteTitle);
 
 }
