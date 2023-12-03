e765eaaeda02b52a0cc63142f568b637a4ca3d4c
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-03 05:21:15 -0500

    implemented deleteinputdata and renameNoteinput data testing

--- src/test/java/use_case/delete_note/DeleteNoteInputDataTest.java
+++ src/test/java/use_case/delete_note/DeleteNoteInputDataTest.java
@@ -1,7 +1,36 @@
 package use_case.delete_note;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class DeleteNoteInputDataTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class DeleteNoteInputDataTest {
+
+    @Test
+    void testConstructorAndGetNoteID() {
+        // Test data
+        int noteID = 123;
+
+        // Create an instance of DeleteNoteInputData using the constructor
+        DeleteNoteInputData deleteNoteInputData = new DeleteNoteInputData(noteID);
+
+        // Verify that the note ID is correctly set by the constructor
+        assertEquals(noteID, deleteNoteInputData.getNoteID());
+    }
+
+    @Test
+    void testGetNoteIDWithDifferentValue() {
+        // Test data
+        int noteID = 123;
+        int differentNoteID = 456;
+
+        // Create an instance of DeleteNoteInputData using the constructor
+        DeleteNoteInputData deleteNoteInputData = new DeleteNoteInputData(noteID);
+
+        // Verify that the note ID is correctly set by the constructor
+        assertEquals(noteID, deleteNoteInputData.getNoteID());
+
+        // Verify that the note ID is not equal to a different value
+        assertEquals(differentNoteID != deleteNoteInputData.getNoteID(), true);
+    }
 }
