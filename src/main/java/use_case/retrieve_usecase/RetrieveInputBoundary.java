c5dd4c3680ce8a00926effe2e6021265bd26fc32
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-03 08:32:14 -0500

    created file for RetrieveInteractor, have yet to implement the use case fully.

--- src/main/java/use_case/retrieve_usecase/RetrieveInputBoundary.java
+++ src/main/java/use_case/retrieve_usecase/RetrieveInputBoundary.java
@@ -4,5 +4,5 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public interface RetrieveInputBoundary {
-    ArrayList execute() throws IOException;
+    void execute() throws IOException;
 }
