c7212f42d10a6aa705138bb5105a440d46f3d364
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-03 05:21:44 -0500

    Implemented rest of rename Note testing

--- src/test/java/use_case/rename_note/RenameNoteOutputDataTest.java
+++ src/test/java/use_case/rename_note/RenameNoteOutputDataTest.java
@@ -1,7 +1,18 @@
 package use_case.rename_note;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class RenameNoteOutputDataTest {
+public class RenameNoteOutputDataTest {
+
+    @Test
+    void testGetTitle() {
+        // Create an instance of RenameNoteOutputData with a title
+        RenameNoteOutputData renameNoteOutputData = new RenameNoteOutputData("New Note Title");
+
+        // Use assertEquals to verify that the getTitle method returns the expected title
+        assertEquals("New Note Title", renameNoteOutputData.getTitle());
+
+    }
 
 }
