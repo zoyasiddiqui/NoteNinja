4e1a9563f2570bad73fe44d429f4806eaa0f71e6
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-02 21:58:57 -0500

    added comments for edit_note

--- src/main/java/use_case/edit_note/EditNoteOutputBoundary.java
+++ src/main/java/use_case/edit_note/EditNoteOutputBoundary.java
@@ -1,15 +1,21 @@
+// Package declaration indicating the location of the interface within the project structure
 package use_case.edit_note;
 
-import use_case.create_AI_snippet.CreateAISnippetOutputData;
-import use_case.create_code_snippet.CreateCodeSnippetOutputData;
+// Import statement for the output data class from a different package
 import use_case.rename_note.RenameNoteOutputData;
-import use_case.search_notes.SearchOutputData;
 
+// Definition of the EditNoteOutputBoundary interface
 public interface EditNoteOutputBoundary {
+
+    // Declaration of methods without implementation details
+    // These methods are expected to be implemented by classes that implement this interface
+
+    // Prepare for displaying note content
     void prepareNote(EditNoteOutputData note);
 
+    // Prepare for displaying title change
     void prepareTitleChange(RenameNoteOutputData title);
 
+    // Prepare for navigating back to the menu
     void prepareBackMenu();
-
 }
