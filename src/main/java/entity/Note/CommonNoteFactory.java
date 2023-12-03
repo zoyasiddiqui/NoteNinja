a1167824707ccbd04c9ce2847d818e0d7878b9d2
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-03 03:17:32 -0500

    removed print statement that was added for debugging in CommonNoteFactory

--- src/main/java/entity/Note/CommonNoteFactory.java
+++ src/main/java/entity/Note/CommonNoteFactory.java
@@ -9,9 +9,6 @@ public class CommonNoteFactory implements NoteFactory {
     // Implementation of the create method from the NoteFactory interface
     @Override
     public Note create(String name, String text, int noteID) {
-        // Log the noteID to the console (for demonstration purposes)
-        System.out.println(noteID);
-
         // Create and return a new instance of CommonNote with the provided parameters
         return new CommonNote(name, text, noteID);
     }
