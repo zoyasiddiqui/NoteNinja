c93714f12b255bf3d4be2f548dac642383dbc403
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-03 17:32:07 -0500

    Updated tests for HomeViewUsecase factory to reflect changes

--- src/test/java/app/HomeViewUseCaseFactoryTest.java
+++ src/test/java/app/HomeViewUseCaseFactoryTest.java
@@ -1,108 +1,42 @@
 package app;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import entity.Note.NoteFactory;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.create_note.CreateNoteController;
-import interface_adapter.create_note.CreateNoteViewModel;
-import interface_adapter.edit_note.EditNotePresenter;
 import interface_adapter.edit_note.EditNoteViewModel;
-import interface_adapter.search_notes.SearchController;
 import interface_adapter.search_notes.SearchViewModel;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import use_case.create_note.CreateNoteDataAccessInterface;
 import use_case.search_notes.SearchNotesAccessInterface;
 import view.HomeView;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
+
 public class HomeViewUseCaseFactoryTest {
 
     @Test
-    void testCreateEditNotePresenter() {
+    void testCreateHomeView() throws IOException {
         // Mock dependencies
-        CreateNoteViewModel createNoteViewModel = Mockito.mock(CreateNoteViewModel.class);
-        EditNoteViewModel editNoteViewModel = Mockito.mock(EditNoteViewModel.class);
-        ViewManagerModel viewManagerModel = Mockito.mock(ViewManagerModel.class);
+        ViewManagerModel viewManagerModel = mock(ViewManagerModel.class);
+        EditNoteViewModel editNoteViewModel = mock(EditNoteViewModel.class);
+        SearchViewModel searchViewModel = mock(SearchViewModel.class);
 
-        // Create an instance of EditNotePresenter using the helper method
-        EditNotePresenter editNotePresenter = HomeViewUseCaseFactory.createEditNotePresenter(
-                createNoteViewModel,
-                editNoteViewModel,
-                viewManagerModel
-        );
+        // Use Mockito to create a mock that implements both interfaces
+        SearchNotesAccessInterface searchNotesAccessInterface = mock(SearchNotesAccessInterface.class, withSettings().extraInterfaces(CreateNoteDataAccessInterface.class));
 
-        // Assert that the EditNotePresenter is not null
-        assertNotNull(editNotePresenter);
-    }
-
-    @Test
-    void testCreateCreateNoteController() {
-        // Mock dependencies
-        CreateNoteViewModel createNoteViewModel = Mockito.mock(CreateNoteViewModel.class);
-        EditNoteViewModel editNoteViewModel = Mockito.mock(EditNoteViewModel.class);
-        ViewManagerModel viewManagerModel = Mockito.mock(ViewManagerModel.class);
-        SearchViewModel searchViewModel = Mockito.mock(SearchViewModel.class);
-        CreateNoteDataAccessInterface noteDataAccessObject = Mockito.mock(CreateNoteDataAccessInterface.class);
-        NoteFactory noteFactory = Mockito.mock(NoteFactory.class);
-
-        // Create an instance of CreateNoteController using the helper method
-        CreateNoteController createNoteController = HomeViewUseCaseFactory.createCreateNoteController(
-                createNoteViewModel,
-                editNoteViewModel,
-                viewManagerModel,
-                searchViewModel,
-                noteDataAccessObject,
-                noteFactory
-        );
-
-        // Assert that the CreateNoteController is not null
-        assertNotNull(createNoteController);
-    }
-
-    @Test
-    void testCreateSearchController() {
-        // Mock dependencies
-        CreateNoteViewModel createNoteViewModel = Mockito.mock(CreateNoteViewModel.class);
-        EditNoteViewModel editNoteViewModel = Mockito.mock(EditNoteViewModel.class);
-        SearchViewModel searchViewModel = Mockito.mock(SearchViewModel.class);
-        ViewManagerModel viewManagerModel = Mockito.mock(ViewManagerModel.class);
-        SearchNotesAccessInterface searchNotesAccessInterface = Mockito.mock(SearchNotesAccessInterface.class);
-
-        // Create an instance of SearchController using the helper method
-        SearchController searchController = HomeViewUseCaseFactory.createSearchController(
-                createNoteViewModel,
-                editNoteViewModel,
-                searchViewModel,
-                viewManagerModel,
-                searchNotesAccessInterface
-        );
-
-        // Assert that the SearchController is not null
-        assertNotNull(searchController);
-    }
-
-    @Test
-    void testCreateHomeView() {
-        // Mock dependencies
-        CreateNoteViewModel createNoteViewModel = Mockito.mock(CreateNoteViewModel.class);
-        EditNoteViewModel editNoteViewModel = Mockito.mock(EditNoteViewModel.class);
-        SearchViewModel searchViewModel = Mockito.mock(SearchViewModel.class);
-        ViewManagerModel viewManagerModel = Mockito.mock(ViewManagerModel.class);
-        CreateNoteDataAccessInterface noteDataAccessObject = Mockito.mock(CreateNoteDataAccessInterface.class);
-        SearchNotesAccessInterface searchNotesAccessInterface = Mockito.mock(SearchNotesAccessInterface.class);
-
-        // Create an instance of HomeView using the factory method
+        // Call the method under test
         HomeView homeView = HomeViewUseCaseFactory.create(
                 viewManagerModel,
-                createNoteViewModel,
                 editNoteViewModel,
                 searchViewModel,
-                noteDataAccessObject,
-                searchNotesAccessInterface
+                (CreateNoteDataAccessInterface) searchNotesAccessInterface
         );
 
         // Assert that the HomeView is not null
         assertNotNull(homeView);
+
+        // Add more assertions as needed
     }
 }
