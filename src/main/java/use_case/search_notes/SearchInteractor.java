e646ed5998177fed21c652d925c17ec74443dac4
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-03 08:25:41 -0500

    SearchInteractor.execute() now sends the editNoteOutputData to searchNotePresenter.prepareExistingNote() instead of editPresenter.prepareNote().

--- src/main/java/use_case/search_notes/SearchInteractor.java
+++ src/main/java/use_case/search_notes/SearchInteractor.java
@@ -3,7 +3,7 @@ package use_case.search_notes;
 
 // Import statements for handling IOException, Note entity, and output boundaries
 import entity.Note.Note;
-import use_case.edit_note.EditNoteOutputBoundary;
+import use_case.search_notes.SearchOutputBoundary;
 import use_case.edit_note.EditNoteOutputData;
 
 import java.io.IOException;
@@ -11,15 +11,15 @@ import java.io.IOException;
 // Definition of the SearchInteractor class implementing the SearchInputBoundary interface
 public class SearchInteractor implements SearchInputBoundary {
 
-    // Instance variables for the searchNotesAccessInterface and editNotePresenter
+    // Instance variables for the searchNotesAccessInterface and searchNotePresenter
     final SearchNotesAccessInterface searchNotesAccessInterface;
-    final EditNoteOutputBoundary editNotePresenter;
+    final SearchOutputBoundary searchNotePresenter;
 
     // Constructor for initializing the SearchInteractor with dependencies
     public SearchInteractor(SearchNotesAccessInterface searchNotesAccessInterface,
-                            EditNoteOutputBoundary editNotePresenter) {
+                            SearchOutputBoundary searchNotePresenter) {
         this.searchNotesAccessInterface = searchNotesAccessInterface;
-        this.editNotePresenter = editNotePresenter;
+        this.searchNotePresenter = searchNotePresenter;
     }
 
     // Implementation of the execute method from the SearchInputBoundary interface
@@ -32,7 +32,7 @@ public class SearchInteractor implements SearchInputBoundary {
         if (note != null) {
             // Prepare output data and notify the presenter
             EditNoteOutputData editNoteOutputData = new EditNoteOutputData(note.getID(), note.getName(), note.getText());
-            editNotePresenter.prepareNote(editNoteOutputData);
+            searchNotePresenter.prepareExistingNote(editNoteOutputData);
         }
     }
 }
