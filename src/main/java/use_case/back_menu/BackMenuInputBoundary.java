7a531d9694de6dff3eae49ebe11bd7d88cb127cf
Saadullah Shahzad <saad.shahzad@mail.utoronto.ca>
2023-12-02 21:35:51 -0500

    Added comments for back_menu use Case

--- src/main/java/use_case/back_menu/BackMenuInputBoundary.java
+++ src/main/java/use_case/back_menu/BackMenuInputBoundary.java
@@ -1,5 +1,10 @@
+// Package declaration indicating the location of the interface within the project structure
 package use_case.back_menu;
 
+// Definition of the BackMenuInputBoundary interface
 public interface BackMenuInputBoundary {
+
+    // Declaration of the execute method without implementation details
+    // This method is expected to be implemented by classes that implement this interface
     public void execute();
 }
