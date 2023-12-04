c0af5e6e16fb9a4f271cbe81b268965ba3695956
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-04 08:00:30 -0500

    modified setNotes method in SearchState and changed notes attribute to be a hashmap mapping noteIDs to their titles

--- src/main/java/interface_adapter/search_notes/SearchState.java
+++ src/main/java/interface_adapter/search_notes/SearchState.java
@@ -3,30 +3,31 @@ package interface_adapter.search_notes;
 import entity.Note.Note;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 // Class declaration for SearchState
 public class SearchState {
     // Instance variable for storing a list of notes
-    private List<Note> notes;
+    private HashMap<Integer, String> notes;
 
     // Copy constructor for SearchState
     public SearchState(SearchState copy) {
-        this.notes = new ArrayList<>(copy.notes);
+        this.notes = copy.notes;
     }
 
     // Default constructor for SearchState
     public SearchState() {
-        this.notes = new ArrayList<>();
+        this.notes = new HashMap<>();
     }
 
     // Getter for the list of notes
-    public List<Note> getNotes() {
+    public HashMap<Integer, String> getNotes() {
         return this.notes;
     }
 
     // Setter for the list of notes
-    public void setNotes(List<Note> notes) {
+    public void setNotes(HashMap<Integer, String> notes) {
         this.notes = notes;
     }
 }
