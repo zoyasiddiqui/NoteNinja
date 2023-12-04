149b5eab9929e486ccf22fe38b01965627f0b572
tanishboiiii <tanishroy1963@gmail.com>
2023-12-04 02:56:40 -0500

    created RetrieveOutputData, have yet to implement it

---
+++ src/main/java/use_case/retrieve/RetrieveOutputData.java
@@ -0,0 +1,14 @@
+package use_case.retrieve;
+
+import entity.Note.Note;
+
+import java.util.ArrayList;
+
+public class RetrieveOutputData {
+    private final ArrayList<Note> notes;
+    RetrieveOutputData (ArrayList<Note> notes) {
+    // TODO: figure out the parameters
+        this.notes = notes;
+    }
+}
+
