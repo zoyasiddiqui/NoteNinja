32009f0aeccc12e92d6f2bf18fea95b379fb1265
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-04 08:01:35 -0500

    we now take in an arraylist called noteaArray from noteDAO.getNotes() and turn it into a respective notes hashmap mapping note IDs to their titles.

--- src/main/java/use_case/retrieve/RetrieveInteractor.java
+++ src/main/java/use_case/retrieve/RetrieveInteractor.java
@@ -5,6 +5,7 @@ import use_case.search_notes.SearchNotesAccessInterface;
 import use_case.search_notes.SearchOutputBoundary;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RetrieveInteractor implements RetrieveInputBoundary {
     private final SearchOutputBoundary searchNotePresenter;
@@ -16,7 +17,11 @@ public class RetrieveInteractor implements RetrieveInputBoundary {
 
     @Override
     public void execute() {
-        ArrayList<Note> notes = noteDataAccessObject.getNotes();
+        ArrayList<Note> noteArray = noteDataAccessObject.getNotes();
+        HashMap<Integer, String> notes = new HashMap<>(); // hashmap <noteID, noteTitle>
+        for (Note note: noteArray) {
+            notes.put(note.getID(), note.getName());
+        }
         RetrieveOutputData retrieveOutputData = new RetrieveOutputData(notes);
         searchNotePresenter.loadOptions(retrieveOutputData);
     }
